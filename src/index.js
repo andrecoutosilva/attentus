24213b421d549bc0bf5964fa0f402c0f6d8d1a06
Andre Silva <andrecoutosilva@gmail.com>
2018-09-23 23:04:21 +0100

    Added popper and jquery to make the Menu collapse on mobile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css'
+
+import './styles/index.css';
+import './include/bootstrap';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
